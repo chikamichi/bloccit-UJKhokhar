e752da63a85b02b66291218cf8316880ea4630f3
ujkhokhar <ujkhokhar@gmail.com>
2015-02-20 15:47:28 -0500

    Create a contacts page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Bloccit::Application.routes.draw do
   get "welcome/index"
   get "welcome/about"
+  get "welcome/contact"
 
     root to: 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
