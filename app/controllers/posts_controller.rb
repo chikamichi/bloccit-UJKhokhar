d4a0f28916cc1dc91008178a921f4307bd2b8852
Jean-Denis Vauguet <jd@vauguet.fr>
2015-02-27 12:23:35 +0100

    Code cleanup
    Signed-off-by: Jean-Denis Vauguet <jd@vauguet.fr>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,44 +1,43 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-      authorize @posts
+    authorize @posts
   end
 
   def show
-    #ApplicationController::Find
     @post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
-      authorize @post
+    authorize @post
   end
-  
-    def create
-      @post = current_user.posts.build(params.require(:post).permit(:title, :body))
-        authorize @post
-      if @post.save
-        flash[:notice] = "Post was saved."
-        redirect_to @post
-      else
-        flash[:error] = "There was an error saving the post. Please try again."
-        render :new
-      end
-    end
 
+  def create
+    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
 
   def edit
     @post = Post.find(params[:id])
-      authorize @post
+    authorize @post
   end
 
-    def update 
-      @post = Post.find(params[:id])
-      if @post.update_attributes(params.require(:post).permit(:title, :body))
-        flash[:notice] = "Post was updated."
-        redirect_to @post
-      else
-        flash[:error] = "There was an error saving the post. Please try again."
-      end
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(params.require(:post).permit(:title, :body))
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
     end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,8 @@ class User < ActiveRecord::Base
   def admin?
    role == 'admin'
   end
- 
+
   def moderator?
     role == 'moderator'
   end
-
 end
