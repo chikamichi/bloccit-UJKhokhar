d7756c69fb4c9a23d9d91e8bf08bbc37837c32fe
Jean-Denis Vauguet <jd@vauguet.fr>
2015-02-27 12:42:09 +0100

    PostPolicy::Scope
    * Anonymous users should not see any posts.
    * Moderators and admins should see all posts.
    * Regular, logged-in user should see own posts.
    
    Signed-off-by: Jean-Denis Vauguet <jd@vauguet.fr>

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,6 +1,20 @@
 class ApplicationPolicy
   attr_reader :user, :record
 
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    # The default scope simply resolves to all records.
+    def resolve
+      scope.all
+    end
+  end
+
   def initialize(user, record)
     @user = user
     @record = record
