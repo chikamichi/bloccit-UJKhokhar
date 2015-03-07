46967b28fd1f89055c84642cca825b3ef2d16079
ujkhokhar <ujkhokhar@gmail.com>
2015-03-07 04:45:42 -0500

    Add comment authorization.
    * Create comment policy file that inherits from application policy.
    * Allow users to comment if user is present.
    * Add authorization for comments in comments#create
    * Render comment creation form only if comment authorization passes.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,11 @@
 class CommentsController < ApplicationController
-  # def new
-  #   @topic = Topic.find(params[:topic_id])
-  #   @post = Post.find(params[:post_id])
-  #   @comment = Comment.new
-  # end
 
   def create
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
     @comment = current_user.comments.build(comment_params)
     @comment.post = @post
+    authorize @comment
     if @comment.save
       flash[:notice] = "Comment was saved."
       redirect_to [@topic, @post]
--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20150306064556) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "topic_id"
-    t.string   "image"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
