ba9004eeb842a66933bc79bb6a9d24a195d38adf
ujkhokhar <ujkhokhar@gmail.com>
2015-03-07 22:35:26 -0500

    Fix vote rspec test.
    * Change local variables to instance variables for access throughout test.

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -4,26 +4,26 @@ describe Vote  do
   describe "validations" do
 
     before do
-      up_vote = Vote.new(value: 1)
-      down_vote = Vote.new(value: -1)
-      invalid_vote = Vote.new(value: 5)
+      @up_vote = Vote.new(value: 1)
+      @down_vote = Vote.new(value: -1)
+      @invalid_vote = Vote.new(value: 5)
     end
 
     describe "up_vote#valid?" do
       it "only allows 1 or -1 as a value" do
-        expect( up_vote.valid? ).to eq(true)
+        expect( @up_vote.valid? ).to eq(true)
       end
     end
 
     describe "down_vote#valid?" do
       it "only allows 1 or -1 as a value" do
-        expect( down_vote.valid?).to eq(true)
+        expect( @down_vote.valid?).to eq(true)
       end
     end
 
     describe "invalid_vote#valid?" do
       it "only allows 1 or -1 as a value" do
-        expect( invalid_vote.valid? ).to eq(false)
+        expect( @invalid_vote.valid? ).to eq(false)
       end
     end
 
