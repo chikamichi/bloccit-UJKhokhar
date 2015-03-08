ec0c14ce666708790d8cf5ad828b380e98103121
Jean-Denis Vauguet <jd@vauguet.fr>
2015-03-08 10:25:34 +0100

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -55,12 +55,12 @@ ActiveRecord::Schema.define(version: 20150306064556) do
 
   create_table "users", force: true do |t|
     t.string   "name"
-    t.string   "email",                  default: "",        null: false
-    t.string   "encrypted_password",     default: "",        null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,         null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 20150306064556) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "role",                   default: "regular"
+    t.string   "role"
     t.string   "avatar"
   end
 
