bd665a32e6823374d057b06507cd7c0226b127d4
Jean-Denis Vauguet <jd@vauguet.fr>
2015-03-08 19:13:39 +0100

    Use ActiveSupport::Concern

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ActiveRecord::Base
-  extend Paginate
+  include Paginate
 
   has_many :posts, dependent: :destroy
 end
--- lib/paginate.rb
+++ lib/paginate.rb
@@ -1,7 +1,13 @@
+require 'active_support/concern'
+
 module Paginate
-  def paginate(options)
-    page = options[:page] || 0
-    per_page = options[:per_page] || 10
-    self.limit(options[:per_page]).offset(page * per_page)
+  extend ActiveSupport::Concern
+
+  included do
+    def self.paginate(options)
+      page = options[:page] || 0
+      per_page = options[:per_page] || 10
+      self.limit(options[:per_page]).offset(page * per_page)
+    end
   end
 end
