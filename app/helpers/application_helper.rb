2526916f35efce09e04e9a45eae1b8a7eca437a9
Jean-Denis Vauguet <jd@vauguet.fr>
2015-03-08 23:29:34 +0100

    Custom will_paginate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,47 @@ module ApplicationHelper
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
   end
+
+  # Paginate a collection of records: create a navigation bar with links to
+  # previous/next pages.
+  #
+  # Same API than official will_paginate: https://github.com/mislav/will_paginate/blob/master/lib/will_paginate/view_helpers/action_view.rb#L26
+  # but with less features (no inference, basic rendering…)
+  #
+  # @param collection [ActiveRecord::Relation] a collection of records
+  def will_paginate(collection = nil, options = {})
+    html = ""
+    current_page = collection.pagination[:current_page]
+    total_pages = collection.pagination[:total_pages]
+
+    # Don't display pagination links if the collection is not paginated,
+    # or there is only one page.
+    return html if collection.nil?
+    return html if total_pages == 1
+
+    # If this is not the first page being displayed, add a link to the previous
+    # page.
+    if current_page > 0
+      html << content_tag(:span, link_to("<- Previous", topics_path(page: current_page - 1)))
+    end
+
+    # Display a direct link for each page (but the current one).
+    total_pages.times do |page|
+      if page == current_page
+        html << content_tag(:span, page, class: 'current')
+      else
+        html << content_tag(:span, link_to(page, topics_path(page: page)))
+      end
+    end
+
+    # If this is not the last page being displayed, add a link to the next
+    # page.
+    if current_page < total_pages - 1
+      html << content_tag(:span, link_to("Next ->", topics_path(page: current_page + 1)))
+    end
+
+    content_tag(:div, class: 'pagination') do
+      html.html_safe
+    end.html_safe
+  end
 end
--- app/models/concerns/pagination.rb
+++ app/models/concerns/pagination.rb
@@ -2,9 +2,19 @@ module Pagination
   extend ActiveSupport::Concern
 
   included do
+    class << self
+      attr_accessor :pagination
+    end
+
     def self.paginate(options = {})
-      page = options[:page] || 0
-      per_page = options[:per_page] || 10
+      page = options[:page].to_i || 0
+      per_page = options[:per_page].to_i || 10
+      self.pagination = {
+        current_page: page,
+        per_page: per_page,
+        total_records: self.count,
+        total_pages: (self.count.to_f / per_page).ceil
+      }
       self.limit(per_page).offset(page * per_page)
     end
   end
