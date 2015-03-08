18f65f2161de9dfc79d42d8be54281202519a92a
ujkhokhar <ujkhokhar@gmail.com>
2015-03-08 05:14:53 -0400

    Refactor routing to make it shallow and adjust comment paths.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Bloccit::Application.routes.draw do
 
-  get "comments/create"
   devise_for :users
   resources :users, only: [:update]
 
   resources :topics do
-    resources :posts, except: [:index] do
-      resources :comments, only: [:create, :destroy]
-    end
+    resources :posts, except: [:index]
   end
+
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
+
+
 
   resources :summary, only: [:new, :create, :show]
 
