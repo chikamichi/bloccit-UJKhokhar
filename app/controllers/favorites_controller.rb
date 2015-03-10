39992987927206573eed277ef1a05eb1cc0e354f
ujkhokhar <ujkhokhar@gmail.com>
2015-03-10 06:29:25 -0400

    Add favorite#create

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,17 @@
 class FavoritesController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.build(post: @post)
+  
+    if favorite.save
+      flash[:notice] = "Post was favorited."
+      redirect_to [@post.topic, @post]
+    else
+      flash[:error] = "Favoriting failed. Please try again."
+      redirect_to [@post.topic, @post]
+    end
+
+  end
+
 end
