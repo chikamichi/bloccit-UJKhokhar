5ea5771ff4b13e65bb2bcff35b5f2c9ef3cee2b8
ujkhokhar <ujkhokhar@gmail.com>
2015-03-13 01:20:18 -0400

    Display a users info, posts and comments on their profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:show]
 
-  def show 
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+    @comments = @user.comments 
   end
 
   def update
