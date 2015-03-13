46f69ee633b2af2bb08fc3fa54a0e14cc7a6467e
ujkhokhar <ujkhokhar@gmail.com>
2015-03-13 03:09:29 -0400

    Test for comment path helper when user is logged in.

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
+include Warden::Test::Helpers
+Warden.test_mode!
+
 describe "Visiting profiles" do
 
   include TestFactories
@@ -7,7 +10,7 @@ describe "Visiting profiles" do
   before do
     @user = authenticated_user
     @post = associated_post(user: @user)
-    @comment = Comment.new(user: @user, body: "A Comment")
+    @comment = @post.comments.build(user: @user, body: "A Comment")
     allow(@comment).to receive(:send_favorite_emails)
     @comment.save
   end
@@ -21,6 +24,23 @@ describe "Visiting profiles" do
       expect( page ).to have_content(@user.name)
       expect( page ).to have_content(@post.title)
       expect( page ).to have_content(@comment.body)
+    end
+
+  end
+
+  describe "signed in" do
+
+    before do
+      login_as(@user, :scope => :user)
+    end
+
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@post.title)
     end
 
   end
